8a5375230da5fd264eb024f5de5b9b6e95291ada
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-17 14:49:31 +0100

    Exercice completé

---
+++ Variables - Exercice 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables___Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String civilite, prenom;
+            int visites;
+
+            civilite = "Monsieur";
+            prenom = "Vincent";
+            visites = 1;
+
+            Console.WriteLine("Bonjour " + civilite + " " + prenom + ", vous êtes venu nous rendre visite " + visites + " fois");
+        }
+    }
+}
